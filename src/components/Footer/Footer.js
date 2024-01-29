3a696c032b2ddc4dea4f825f481a451f16e5b0d6
Naom Ricardo <78932401+NaomR@users.noreply.github.com>
2024-01-29 17:10:55 -0300

    clone spotify

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import './Footer.css';
+
+const Footer = () => {
+   return (
+    <footer className="disclaimer-premium">
+    <div className="text">
+      <p className="disclaimer-premium__title">Testar o Premium de graça</p>
+      <p className="disclaimer-premium__subtitle">
+        Inscreva-se para curtir música ilimitada e podcasts só com alguns
+        anuncios.Não precisa de cartão de crédito.
+      </p>
+    </div>
+    <div className="button">
+      <button type="button">Inscreva-se Grátis</button>
+    </div>
+  </footer>
+   )
+}
+
+export default Footer;
